269fdcfb310964cfbe3fa4cfc1368061cf7529f3
anirudt <anirudt@gmail.com>
2015-09-24 13:29:25 +0530

    first working model

--- notify.py
+++ notify.py
@@ -2,8 +2,9 @@
 import os
 import sys
 import optparse
+import time
 
-desc = "The current Python file is notified with crontab every second. This file has the intelligence to recognise the appropriate notifier to notify the user depending upon the minute. Currently, we support only one flag at a time."
+desc = "The current Python file is notified with crontab every minute. This file has the intelligence to recognise the appropriate notifier to notify the user depending upon the minute. Currently, we support only one flag at a time."
 
 
 # Option Parser Object Creation
@@ -11,12 +12,65 @@ p = optparse.OptionParser(description=desc)
 
 
 # HELPER FUNCTIONS
+def parse_min(mn, time_struct):
+    if mn=="*":
+        return 1
+    else: 
+        if mn.find('/')!=-1:
+            num = int(mn[mn.find('/'):-1])
+            if num%time_struct.tm_min==0:
+                return 1
+            else:
+                return 0
+        else:
+            num = int(mn)
+            if num == time_struct.tm_min:
+                return 1
+            else:
+                return 0
+
+
+
+def parse_hr(hr, time_struct):
+    if hr=="*":
+        return 1
+    else: 
+        if hr.find('/')!=-1:
+            num = int(hr[hr.find('/'):-1])
+            if num%time_struct.tm_hour==0:
+                return 1
+            else:
+                return 0
+        else:
+            num = int(hr)
+            if num == time_struct.tm_hour:
+                return 1
+            else:
+                return 0
+
+def parse_day(dy, time_struct):
+    if dy=="*":
+        return 1
+    else: 
+        if dy.find('/')!=-1:
+            num = int(dy[dy.find('/'):-1])
+            if num%time_struct.tm_mday==0:
+                return 1
+            else:
+                return 0
+        else:
+            num = int(dy)
+            if num == time_struct.tm_mday:
+                return 1
+            else:
+                return 0
+
 def addOptions():
     p.add_option('-s','--show',dest='show',default=False,action='store_true',help='lists the commands present')
     p.add_option('-r','--reg',dest='reg',help='registers new command')
 
 
-def decideCmd(opts):
+def decideCmd(opts,time_struct):
     if opts.show:
         ''' Enlisting the commands '''
         print "============================================================"
@@ -39,12 +93,20 @@ def decideCmd(opts):
                 a1.append(words[0])
                 a2.append(words[1])
                 a3.append(words[2])
-                cmd.append(words[-1])
-        print a1, a2, a3, cmd
+                cmd.append(' '.join(words[5:]))
 
-#os.system("/usr/bin/notify-send"+string)
+                # Join the cmd words here
+        i = 0
+        while i<len(a1):
+            ret1 = parse_min(a1[i], time_struct) 
+            ret2 = parse_hr(a2[i], time_struct)
+            ret3 = parse_day(a3[i], time_struct)
+            if ret1+ret2+ret3 == 3:
+                os.system("/usr/bin/notify-send '"+cmd[i]+"'");
+            i+=1
 
 if __name__ == "__main__":
     addOptions()
     (opts, args) = p.parse_args()
-    decideCmd(opts)
+    localtime = time.localtime(time.time())
+    decideCmd(opts,localtime)
